79d8af11d39311c6d88bfec291cc9fccf5b875d8
Christian <alvescastro00cca13@gmail.com>
2020-11-18 10:40:49 -0300

    Adicionando as configurações do banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,23 +1,14 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+module.exports = {
+  host: 'localhost',
+  username: 'pguser',
+  password: 'pgpassword',
+  database: 'nodeauth',
+  dialect: 'postgres',
+  operatorsAliases: false,
+  logging: false,
+  define:{
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
   }
 }
