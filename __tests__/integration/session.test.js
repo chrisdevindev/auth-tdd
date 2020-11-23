5977938e720a52823bdb596f387584e8df0a01e3
Christian <alvescastro00cca13@gmail.com>
2020-11-23 12:36:33 -0300

    Fazendo testes de credenciais válidas e inválidas

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,36 +1,47 @@
-const request = require('supertest')
-const app = require('../../src/app')
-const { User } = require('../../src/app/models')
-const truncate = require('../utils/truncate')
+const request = require("supertest");
+const app = require("../../src/app");
+const { User } = require("../../src/app/models");
+const truncate = require("../utils/truncate");
 
 /**
  *  beforeAll()  -> Executa automaticamente uma vez antes de todos os testes desse arquivo
  *  afterAll()   -> Executa automaticamente uma vez depois de todos os testes desse arquivo
- *  beforeEach() -> Executa antes de cada um dos testes 
- *  afterEach()  -> Executa depois de cada um dos testes 
- * 
+ *  beforeEach() -> Executa antes de cada um dos testes
+ *  afterEach()  -> Executa depois de cada um dos testes
+ *
  */
 
-describe('Authentication', () => {
+describe("Authentication", () => {
+  beforeEach(async () => {
+    await truncate();
+  });
 
-    beforeEach(async () => {
-        await truncate()
-    })
-    
-    it('should authenticate with valid credentials', async () => {
-        const user = await User.create({
-            name: 'Chris',
-            email: 'christian.alves@dellead.com',
-            password_hash: '123123'
-        })
+  it("should authenticate with valid credentials", async () => {
+    const user = await User.create({
+      name: "Chris",
+      email: "christian.alves@dellead.com",
+      password: "123123",
+    });
 
-        const respose = await request(app)
-            .post('/sessions')
-            .send({
-                email: user.email,
-                password: '123456'
-            })
-        expect(respose.status).toBe(200)
-    })
-    
-})
+    const respose = await request(app).post("/sessions").send({
+      email: user.email,
+      password: "123123",
+    });
+
+    expect(respose.status).toBe(200);
+  });
+
+  it("should not authenticate with invalid credentials", async () => {
+    const user = await User.create({
+      name: "Chris",
+      email: "christian.alves@dellead.com",
+      password: "123123",
+    });
+
+    const respose = await request(app).post("/sessions").send({
+      email: user.email,
+      password: "132123",
+    });
+    expect(respose.status).toBe(401);
+  });
+});
